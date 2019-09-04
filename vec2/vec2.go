6379fec1dfe5caadd080ace744ca921cd3fe0c3e
Andreas T Jonsson <mail@andreasjonsson.se>
2019-09-04 14:33:26 +0200

    Cleanup

--- vec2/vec2.go
+++ vec2/vec2.go
@@ -2,6 +2,7 @@ package vec2
 
 import (
 	"fmt"
+	"image"
 
 	"github.com/andreas-jonsson/fix16"
 )
@@ -9,7 +10,7 @@ import (
 type T [2]fix16.T
 
 func (v T) String() string {
-	return fmt.Sprintf("[%s, %s]", v[0].String(), v[1].String())
+	return fmt.Sprintf("[%s, %s]", v.X().String(), v.Y().String())
 }
 
 func (v T) X() fix16.T {
@@ -20,20 +21,28 @@ func (v T) Y() fix16.T {
 	return v[1]
 }
 
+func (v T) XY() (fix16.T, fix16.T) {
+	return v.X(), v.Y()
+}
+
+func (v T) YX() (fix16.T, fix16.T) {
+	return v.Y(), v.X()
+}
+
 func (v T) Add(b T) T {
-	return T{v[0].Add(b[0]), v[1].Add(b[1])}
+	return T{v.X().Add(b.X()), v.Y().Add(b.Y())}
 }
 
 func (v T) Sub(b T) T {
-	return T{v[0].Sub(b[0]), v[1].Sub(b[1])}
+	return T{v.X().Sub(b.X()), v.Y().Sub(b.Y())}
 }
 
 func (v T) Mul(b T) T {
-	return T{v[0].Mul(b[0]), v[1].Mul(b[1])}
+	return T{v.X().Mul(b.X()), v.Y().Mul(b.Y())}
 }
 
 func (v T) Div(b T) T {
-	return T{v[0].Div(b[0]), v[1].Div(b[1])}
+	return T{v.X().Div(b.X()), v.Y().Div(b.Y())}
 }
 
 func (v T) Length() fix16.T {
@@ -41,15 +50,15 @@ func (v T) Length() fix16.T {
 }
 
 func (v T) LengthSqr() fix16.T {
-	return v[0].Mul(v[0]).Add(v[1].Mul(v[1]))
+	return v.X().Mul(v.X()).Add(v.Y().Mul(v.Y()))
 }
 
 func (v T) Scale(s fix16.T) T {
-	return T{v[0].Mul(s), v[1].Mul(s)}
+	return T{v.X().Mul(s), v.Y().Mul(s)}
 }
 
 func (v T) Invert() T {
-	return T{-v[0], -v[1]}
+	return T{-v.X(), -v.Y()}
 }
 
 func (v T) Normalize() T {
@@ -62,5 +71,13 @@ func (v T) Normalize() T {
 }
 
 func (v T) Dot(b T) fix16.T {
-	return v[0].Mul(b[0]).Add(v[1].Mul(b[1]))
+	return v.X().Mul(b.X()).Add(v.Y().Mul(b.Y()))
+}
+
+func (v T) Point() image.Point {
+	return image.Pt(v.X().Int(), v.Y().Int())
+}
+
+func (v T) Rectangle() image.Rectangle {
+	return image.Rect(0, 0, v.X().Int(), v.Y().Int())
 }
